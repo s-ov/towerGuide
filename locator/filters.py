2eee64fff5617d8130a7b574f69400d9262fabe9
s-ov <s.ovsiuk@gmail.com>
2024-02-21 21:57:46 +0200

    temporary

--- locator/filters.py
+++ locator/filters.py
@@ -2,10 +2,9 @@ import django_filters
 from .models import Node
 
 class NodeFilter(django_filters.FilterSet):
-    # name = django_filters.CharFilter(lookup_expr='iexact')
 
     class Meta:
         model = Node
         fields = {
-            'slug': ['icontains']
+            'slug': ['exact']
          }
